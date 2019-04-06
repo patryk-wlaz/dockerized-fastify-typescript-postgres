533236de787e5edb67b3c783146e07e7ebf8f74f
patryk-wlaz <patryk.wlaz@gmail.com>
2019-04-06 18:02:52 +0200

    add some general plugins

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
 import * as fastifyFactory from 'fastify';
+import * as fastifyRateLimit from 'fastify-rate-limit';
+import * as helmet from 'fastify-helmet';
+
+// error handler
+// swagger
+// if (!includes(['prod', 'production'], process.env.NODE_ENV)) {
+//   setupSwagger(app);
+// }
 
 const fastify = fastifyFactory({
   logger: true,
 });
+
+// ogólne pluginy (config!)
+fastify.register(fastifyRateLimit, {
+  max: 100,
+  timeWindow: 1000,
+});
+fastify.register(helmet);
+
+// router
 
 fastify.get('/', async (request, reply) => {
   return { hello: 'world' };
